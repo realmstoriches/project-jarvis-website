1047c7e43a369f0d6105f47b019b70cc008c1ecf
realmstoriches <robert.demotto@realmstoriches.xyz>
2025-07-17 01:27:42 -0400

    tailwinds

--- tailwinds.config.js
+++ tailwinds.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}", // Looks for any HTML/JS file in the 'src' folder
-    "./*.html"              // Looks for any HTML file in the main project folder
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./docs/**/*.{html,js}"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
